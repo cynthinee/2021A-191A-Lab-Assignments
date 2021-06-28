2108cdae17f3767047841da49f8f3f816b00b470
cynthinee <cynth.orozco@gmail.com>
2021-06-27 18:22:46 -0700

    broke stuff

--- week1/js/init.js
+++ week1/js/init.js
@@ -7,11 +7,15 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 //JavaScript let variable declaration to create a marker
 let marker = L.marker([34.040562, -118.148308]).addTo(map)
-		.bindPopup('East Los Angeles College<br> is the community college I work at!')
+		.bindPopup('East Los Angeles College<br> I work here!')
 		.openPopup();
 
-let marker2 = L.marker([34.068139, -118.444983]).addTo(map)
-.bindPopup('Santa Monica College<br> is the community college I also work at sometimes')
+let marker2 = L.marker([42.145481, --83.162018]).addTo(map)
+.bindPopup('Loyola Marymount University<br> I worked here!')
+.openPopup();
+
+let marker3 = L.marker([33.790180, ---118.178978]).addTo(map)
+.bindPopup('California State University Long Beach<br> I worked here!')
 .openPopup();
 
 fetch("map.geojson")
